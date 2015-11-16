9fdff55b1fd16faf60656c81ef8e15872e74d561
crb471 <crbertelsen@gmail.com>
2015-11-16 00:28:28 -0600

    incorporating shader, refining visual style

--- js/tunnel.js
+++ js/tunnel.js
@@ -1,20 +1,20 @@
 function Tunnel (initPos, end, audioController) {
 	
-	var offset = 500 
+	var offset = 500; 
 	var numObjects, tunnelRadius;
 	var endTime = end;
 	var audio = audioController;
-	var speed = 10;
+	this.speed = 10;
 	var initialPosition = initPos;
+	this.tunnelRadius = 400;
 
 
 	var objectGeo;
 
 	//init
 
-	var tunnelRadius = 400;
 	var objectGeo = new THREE.PlaneGeometry(500, 400);
-	var objectMat = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide} );
+	//var objectMat = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide} );
 
 	var center = new THREE.Vector3 (0,0,0);
 
@@ -24,9 +24,9 @@ function Tunnel (initPos, end, audioController) {
 	var tunnelRot = Math.random()/100; 
 
 	for (var i = 0; i < numObjects; i++) {
-		var box = new THREE.Mesh(objectGeo, objectMat);
-		box.position.x = tunnelRadius*Math.cos(2*i*Math.PI/numObjects); 
-		box.position.y = tunnelRadius*Math.sin(2*i*Math.PI/numObjects);
+		var box = new THREE.Mesh(objectGeo, tunnelMaterial);
+		box.position.x = this.tunnelRadius*Math.cos(2*i*Math.PI/numObjects); 
+		box.position.y = this.tunnelRadius*Math.sin(2*i*Math.PI/numObjects);
 		box.position.z = 0;
 
 		box.lookAt(center);
@@ -46,17 +46,17 @@ function Tunnel (initPos, end, audioController) {
 		if (time < endTime) {
 			for( var i = 0; i < numObjects; i++) {
 				//thisColor.setHSL(0.6611, 1, 1);
-				this.group.children[i].material.color.setHSL(0.5,audioController.analyzer.array[20]/512,audioController.analyzer.array[20]/1024);
+				//this.group.children[i].material.color.setHSL(0.5,audioController.analyzer.array[20]/512,audioController.analyzer.array[20]/1024);
 				
 			}
 		}
 
 		if (this.group.position.z > (camera.position.z + offset)) {
-			this.group.position.z = camera.position.z - (camera.far+offset);
+			this.group.position.z = camera.position.z - (camera.far+offset+initialPosition);
 			tunnelRot = Math.random()/100
 		}
 
-		this.group.position.z += delta*speed;
+		this.group.position.z += delta*this.speed;
 		this.group.rotation.z += tunnelRot;
 
 	}
